5a1b2e31ed06d4744df69c0cc0ce0edb0265fa8c
ronathan <ronaldvictorino@gmail.com>
2013-09-18 21:37:09 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SynapsABILITY::Application.config.secret_key_base = 'f2e389c98f00c8cef53cbe2576ff42a211c9d16d21bccfc1317eef826746b21c013442538fe75935686bbf35a55939fc1226960d600c0fcddc4078a6be0684db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SynapsABILITY::Application.config.session_store :cookie_store, key: '_synapsABILITY_session'
