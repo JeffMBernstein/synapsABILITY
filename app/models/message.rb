78358f162f91caed56dc1b02d9d0c3e403615e8a
ronathan <ronaldvictorino@gmail.com>
2013-09-25 21:55:33 +0000

    Added tests and validations for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
+
+	validates :subject, :body, :sender_id, presence: true
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "subject cannot be blank" do
+  	message = Message.new(subject: "")
+  	assert message.invalid?
+  	assert message.errors[:subject].any?
+  end
+
+  test "body cannot be blank" do
+  	message = Message.new(body: "")
+  	assert message.invalid?
+  	assert message.errors[:body].any?
+  end
+
+  test "has to be sent to one user" do
+  	message = Message.new(sender_id: nil)
+  	assert message.invalid?
+  	assert message.errors[:sender_id].any?
+  end
 end
