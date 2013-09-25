718aea57077ae74e57c227d9fe077b23bec79090
Jeff Bernstein <jeffreymbernstein@gmail.com>
2013-09-25 21:33:52 +0000

    Added tests for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,16 +19,10 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-     unless @user == @current_user
-      redirect_to user_path(@current_user)
-    end
   end
 
   def edit
     @user = User.find(params[:id])
-    unless @user == @current_user
-      redirect_to edit_user_path(@current_user)
-    end
   end
 
   def update
@@ -42,10 +37,9 @@ class UsersController < ApplicationController
     redirect_to new_user_path
   end
 
-
 private
 
   def user_params
-  	params.require(:user).permit(:first_name, :last_name, :username, :email, :password_digest, :bio, :avatar)
+  	params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation, :bio, :avatar)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,79 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get index" do
+  	get :index
+  	assert_response :success
+  end
+
+  test "should get new" do
+  	get :new
+  	assert_response :success
+  end
+
+  test "should get edit" do
+  	get :edit, id: users(:one).id
+  	assert_response :success
+  end
+
+  test "should get show" do
+  	get :show, id: users(:one).id
+  	assert_response :success
+  end
+
+  test "should create a new user with valid attributes" do
+  	attributes = valid_user_attributes
+  	assert_difference('User.count') do
+  		post :create, user: attributes
+  	end
+    assert_redirected_to users_path
+  end
+
+  test "should NOT create a new user with invalid attributes" do
+    attributes = invalid_user_attributes
+    assert_difference('User.count', 0) do
+      post :create, user: attributes
+    end
+    assert_template :new
+  end
+
+  test "should update a user with valid attributes" do
+    user = users(:two)
+    attributes = valid_user_attributes
+    patch :update, id: user.id, user: attributes
+    user = assigns(:user)
+    assert_equal user.username, "jeffy"
+    assert_redirected_to user_path(user)
+  end
+
+  test "should delete a user" do
+    user = users(:one)
+    assert_difference('User.count', -1) do
+      delete :destroy, id: user.id
+    end
+    assert_redirected_to new_user_path
+  end
+
+  def valid_user_attributes
+    user = users(:one)
+    attributes = user.attributes.except("id")
+    attributes[:password] = "foo"
+    attributes[:password_confirmation] = "foo"
+    user.destroy!
+
+    raise unless User.new(attributes).valid?
+
+    return attributes
+  end
+
+  def invalid_user_attributes
+    attributes = valid_user_attributes
+    attributes[:first_name] = nil
+
+    raise if User.new(attributes).valid?
+
+    return attributes
+  end
+
 end
