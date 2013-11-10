fa3511e504300caec4b24d72ca4a1d3570a0c223
ronathan <ronaldvictorino@gmail.com>
2013-11-10 23:08:00 +0000

    Trying to get comments to work properly, not functional yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+	def new
+		@comment = Comment.new
+		@question = Question.find(params[:id])
+	end
+
+	def create
+		@comment = Comment.new(comment_params)
+		@question = Question.find(params[:id])
+		@comment.question_id = @question.id
+		@comment.save
+		redirect_to questions_path
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:description, :upvote, :downvote, :question_id)
+	end
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,7 @@ class QuestionsController < ApplicationController
 	def index
 		@questions = Question.all
 		@question = Question.new
+		@comment = Comment.new
 	end
 
 	def new
